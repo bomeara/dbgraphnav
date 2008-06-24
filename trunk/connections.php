dccc3ff5f3b524101ec6e1cfe15dbbb0b9608381
DigitalFire <DigitalFire@19c0041d-c44d-0410-952c-05691d09380b>
2008-06-24 00:09:52 +0000

    moved db.php content into connections.php, removed header.php since it's unused.

--- trunk/connections.php
+++ trunk/connections.php
@@ -1,8 +1,33 @@
 <?php
 
-  // CURRENTLY VERY HACKY. clean up later
+  // CURRENTLY somewhat HACKY. clean up later
 
-require_once 'db.php';
+require_once 'Image/GraphViz.php';
+require_once 'MDB2.php';
+require_once 'config/config.php';
+
+class DBGraphNav_DBCon {
+  function get_data($parentnode, $data_type) {
+    $cfg = new DBGraphNav_Config; //broken, decide on config style
+    foreach ($cfg->get_queries($parentnode, $data_type) as $qry){
+      $db =& MDB2::connect($qry["DSN"]);
+      if (PEAR::isError($db)) {
+	die($db->getMessage());
+      }
+      $db->setLimit(5); //debugging, prevents server load
+      $result =& $db->query($qry["query_string"]);
+      if (PEAR::isError($result)) {
+	die($result->getMessage());
+      }
+      
+      while ($row = $result->fetchRow()) {
+	$return[] =$row;
+      }
+    }
+    return $return;
+  }
+}
+
 
 class DBGraphNav_Network {
   //  $network = Array();
@@ -11,9 +36,39 @@ class DBGraphNav_Network {
     $this->db = new DBGraphNav_DBCon;
   }
 
-  function get_network($basenode, $type) {
-    $this->build_network($basenode, $type);
-    return $this->outary;
+  //expects the build_network function to have already been called
+  function get_dot() {
+    $graph = new Image_Graphviz();
+    //type
+    foreach ($this->network as $type=>$value){
+      //actual node
+      foreach ($value as $id=>$value){
+	$cur_node = "\"$type||||$id\"";
+	$graph->addNode(
+			//this creates a unique name for each node. If this
+			//conflicts with a graph's naming scheme, it may be
+			//hashed instead.
+			$cur_node,
+			array(
+			      'label' => $value["display_name"]));
+			      //'url' => 'http://google.com/'));
+	//node neighbor type
+	foreach ($value["neighbors"] as $type) {
+	  //actual node neighbor
+	  foreach ($type as $id=>$display_val){
+	    $graph->addEdge(Array($cur_node => "\"$type||||$id\""));
+	  }
+	}
+      }
+    }
+    $graph->binPath = "/usr/local/bin";
+    echo "here it is:";
+    return $graph->fetch("png");
+  }
+
+  function get_network() {
+    //    $this->build_network($basenode, $type);
+    return $this->network;
   }
 
   function build_network($basenode, $type, $maxdepth = 5, $depth = 0) {
@@ -21,7 +76,7 @@ class DBGraphNav_Network {
     $b = array();
     if ($depth < $maxdepth) {
 	foreach ($this->db->get_data($basenode, $type) as $node) {
-	  $a =& $this->outary[$node[1]][$node[0]];
+	  $a =& $this->network[$node[1]][$node[0]];
 	  if (!isset($a)) { //node info is always the same
 	    $a['display_name']=$node[2];
 	    $a['neighbors'] =
