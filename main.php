72248db01a60cacd913791b7c13e5651cfc178fe
DigitalFire <DigitalFire@19c0041d-c44d-0410-952c-05691d09380b>
2008-06-24 00:09:52 +0000

    moved db.php content into connections.php, removed header.php since it's unused.

--- main.php
+++ main.php
@@ -1,9 +1,10 @@
 <?php
-require_once 'db.php';
 require_once 'connections.php';
 $test = new DBGraphNav_Network;
-print_r($test->get_network(1, "person"));
+$test->build_network(1, "person");
+//print_r($test->get_network());
 //echo $test->get_data("reference", 807);
+$test->get_dot();
 
 
 
