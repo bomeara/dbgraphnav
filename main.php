67328d57ca6a1f0640917642394d13577c5b4ef6
DigitalFire <DigitalFire@19c0041d-c44d-0410-952c-05691d09380b>
2008-07-01 05:40:10 +0000

    hunting bugs in dot file output

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 require_once 'connections.php';
 $test = new DBGraphNav_Network;
 $test->build_network(1, "person");
-//print_r($test->get_network());
+print_r($test->get_network());
 //echo $test->get_data("reference", 807);
 $test->get_dot();
 
