d0dde0017151835fc3fa3f1c7ca94ca0544d21ab
DigitalFire <DigitalFire@19c0041d-c44d-0410-952c-05691d09380b>
2008-07-01 20:03:34 +0000

    graph generation working with url parameters

--- trunk/main.php
+++ trunk/main.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'connections.php';
 $test = new DBGraphNav_Network;
-$test->build_network(1, "person");
-print_r($test->get_network());
+$test->build_network($_REQUEST["id"], $_REQUEST["type"], $_REQUEST["depth"]);
+//print_r($test->get_network());
 //echo $test->get_data("reference", 807);
-$test->get_dot();
+echo $test->get_dot();
 
 
 
