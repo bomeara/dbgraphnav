6b4947870403b9abae296c14a159ff51eb2da274
DigitalFire <DigitalFire@19c0041d-c44d-0410-952c-05691d09380b>
2008-07-06 20:49:36 +0000

    Moved configuration options into config file. Added a few options there. General code cleanup. Singleton config class.

--- main.php
+++ main.php
@@ -1,6 +1,5 @@
 <?php
-require_once 'connections.php';
-$test = new DBGraphNav_Network;
+require_once 'connections.php';$test = new DBGraphNav_Network;
 $test->build_network($_REQUEST["id"], $_REQUEST["type"], $_REQUEST["depth"]);
 //print_r($test->get_network());
 //echo $test->get_data("reference", 807);
