084e00280d92cb6d70d93b1077a11a5f7f6959c6
DigitalFire <DigitalFire@19c0041d-c44d-0410-952c-05691d09380b>
2008-07-08 11:10:57 +0000

    cache work, in prep for clickable image maps

--- main.php
+++ main.php
@@ -1,11 +1,11 @@
 <?php
-require_once 'connections.php';
-$test = new DBGraphNav_Network;
-$test->build_network($_REQUEST["id"], $_REQUEST["type"], $_REQUEST["depth"]);
+require_once 'cache.php';
+$test = new DBGraphNav_Cache;
+$test->graph->build_network($_REQUEST["id"], $_REQUEST["type"], $_REQUEST["depth"]);
 //print_r($test->get_network());
 //echo $test->get_data("reference", 807);
-echo $test->get_dot();
-
+//echo $test->get_image();
+$test->fetch();
 
 
 ?>
