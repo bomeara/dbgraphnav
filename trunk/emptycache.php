c4783b889f0e6defaad9719d6b4d76c756292584
DigitalFire <DigitalFire@19c0041d-c44d-0410-952c-05691d09380b>
2008-07-28 20:38:34 +0000

    documentation, commenting, code cleanup

--- trunk/emptycache.php
+++ trunk/emptycache.php
@@ -1,4 +1,8 @@
 <?php
+  /*This file is intended as an example of how to use the cache
+    clearing functions. It is included because it is often useful when
+    debugging graphing parameters.  */
+
 require_once 'cache.php';
 $cache = new DBGraphNav_Cache();
 echo "Cleared " . $cache->clear() . " files.";
--- trunk/main.php
+++ trunk/main.php
@@ -1,25 +1,45 @@
 <?php
-session_start();
-require_once 'cache.php';
+  /* This file is intended as an example of how to use the facilities
+     provided by DBGraphNav. Feel free to modify it, or create
+     something completely different. */
 
-$cache = new DBGraphNav_Cache;
+  /* We use session (in this example) to store the depth of the
+     displayed graph. Higher values traverse deeper into the displayed
+     tree. Experiment with values, since the appropriate value will
+     depend on how dense your data is. */
+session_start();
 if (isset($_REQUEST["depth"])) {
   $depth = (int)$_REQUEST["depth"];
   $_SESSION["DBGN_depth"] = $depth;
 } elseif (isset($_SESSION["DBGN_depth"])) {
   $depth = (int)$_SESSION["DBGN_depth"];
 } else {
-  $depth = 1;
+  $depth = 1; //a safe, but uninteresting default
 }
 
-//ID NEEDS TO BE ESCAPED! FIX ME SOON. (type does not need escaping)
-$cache->graph->build_network($_REQUEST["id"], $_REQUEST["type"], $depth);
+//this is the only DBGraphNav file needed since it includes the others
+require_once 'cache.php'; 
+$cache = new DBGraphNav_Cache;
+/* Note that DBGraphNav does not do any query escaping for you. The
+   first (ID) field is the only field which must be escaped, but it is
+   done here rather than deeper in the program to improve
+   compatibility with databases which might have non-standard
+   formats. Be sure to use the correct function (pg_escape_string,
+   mysql_escapestring, etc.) for your database type. */
+$id = $cache->graph->build_network(pg_escape_string($_REQUEST["id"]),
+				   $_REQUEST["type"],
+				   $depth);
+/* Returns an array consisting of the age of the cached image (if the
+   image was found in the cache - 0 indicates a fresh image, -1
+   indicates a "complex" cached image [see the docs for more info]),
+   the path to the actual image, and the path to the image map. */
 $result = $cache->fetch();
 
-echo "Cache Age: " . $result['age'] . "<br>";
+//you don't have to display this to users, but it's convenient for debugging
+echo "Cache Age: " . $result['age'] . "<br>"; 
 echo '<img src="';
 echo $result['img'] . '" usemap = "#G" border="0" />';
-//echo image map
-readfile($result['map']);
+readfile($result['map']);//print the image map into the HTML source
+
 
 ?>
