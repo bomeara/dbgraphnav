7607a778d6cf659947077a18f10deda3318d648f
DigitalFire <DigitalFire@19c0041d-c44d-0410-952c-05691d09380b>
2008-07-31 21:32:52 +0000

    working graphviz version check in setup

--- trunk/setup/setup2.php
+++ trunk/setup/setup2.php
@@ -20,14 +20,38 @@ $pear_dependency->callCheckMethod($error, $opts);
 if($error) {
   die( $error . "<br>\n Make sure you installed the correct database driver for MDB2. <br>\n");
 }
-//this part isn't working yet
 
-echo $_REQUEST["gv_path"] . " -V<br>\n";
-//echo shell_exec($_REQUEST["gv_path"] . " -V ");
-echo exec("whoami");
-echo exec("/usr/local/bin/neato -V", $output);
-print_r($output);
-echo "done";
+/*We want to read from the standard error here since graphviz prints
+  version info there, for some reason. */
+$process = proc_open($_REQUEST["gv_path"] . " -V", 
+		     array(2 => array("pipe", "w")),
+		     $pipes);
+if (is_resource($process)){
+  $gv_version = stream_get_contents($pipes[2]);
+  fclose($pipes[2]);
+  proc_close($process);
+  /* We have to actually check for a portion of the graphviz version
+     return string, since this pipe may also contain a "file not
+     found" type error message. */
+  if (strpos($gv_version,"- Graphviz version ") !== FALSE){ 
+    echo "You are running \"<b>$gv_version</b>\".<br>\n";
+    $pos = strpos($gv_version,"- Graphviz version ") + 19;
+    $ver = explode(".", //extract the version number
+		   substr($gv_version, 
+			  $pos, 
+			  strpos($gv_version, " ", $pos) - $pos));
+    if (($ver[0] < 2) or (($ver[0] = 2) and ($ver[1] < 21))) {
+      echo "Your version of Graphviz is pretty old. It is likely to work, but this program was tested with version 2.21.0. Some package managers have a very ancient version (2.16.0), which contains a number of known bugs. An upgrade is strongly recommended.<br>\n";
+    }
+  } else {
+    die("Graphviz not found. You entered the path \"<b>$_REQUEST[gv_path]</b>\". <br>\n Make sure it is installed, make sure your path is correct (you may need to use an absolute path), make sure that php has sufficient rights to execute graphviz.<br>\n");
+  }
+} else {
+  echo "Strange error with proc_open(). This code should never execute. Manually check your graphviz path.";
+}
+
+
+
 ?>
 
 
